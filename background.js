411017f468a81f965a33cb43ab8be772e34d7797
bishal-dahal <101317575+bishal-dahal@users.noreply.github.com>
2023-09-22 14:56:15 +0545

    Fixed the download issue and added content.js for background script helper

--- background.js
+++ background.js
@@ -1,11 +1,17 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.webNavigation.onBeforeNavigate.addListener((details) => {
-        if (details.frameId === 0 && details.url.endsWith('.m3u8')) {
-          const playerUrl = chrome.runtime.getURL('player.html') + '#' + details.url;
-          chrome.tabs.update({ url: playerUrl });
-        }
-      }),
-      { urls: ["<all_urls>"] },
-      ["blocking"]  
-  });
-  
+// chrome.runtime.onInstalled.addListener(() => {
+  chrome.webNavigation.onBeforeNavigate.addListener((details) => {
+    if (details.frameId === 0 && details.url.endsWith('.m3u8')) {
+      const playerUrl = chrome.runtime.getURL('player.html') + '#' + details.url;
+      console.log("Before Navigation", playerUrl);
+      chrome.tabs.create({ url: playerUrl });
+    }
+  }, { urls: ["<all_urls>"] }, ["blocking"]);
+// });
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.command == 'CMD_PLAY_M3U8') {
+    var playerUrl = chrome.runtime.getURL('player.html') + "#" + request.url;
+    console.log("onMessage", playerUrl);
+    chrome.tabs.create({ url: playerUrl });
+  }
+});
