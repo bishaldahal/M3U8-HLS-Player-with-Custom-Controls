fc9efda9d3c199dd1e5245f705479601e59d417a
bishal-dahal <101317575+bishaldahal@users.noreply.github.com>
2024-02-21 21:13:59 +0545

    Refactor keyboard shortcuts documentation

--- shortcuts.js
+++ shortcuts.js
@@ -12,54 +12,51 @@ const keyToPercentage = {
 };
 
 function generateShortcutsDocumentation(shortcuts) {
-    const shortcutsList = document.createElement('ul');
-    shortcuts.forEach(shortcut => {
-      const shortcutListItem = document.createElement('li');
-      const shortcutCode = document.createElement('code');
-      const keyBinding = document.createElement('span');
-      const key = document.createElement('span');
-      key.className = 'key';
-      key.textContent = shortcut.key;
-      keyBinding.className = 'key-binding';
-      keyBinding.appendChild(key);
-      shortcutCode.appendChild(keyBinding);
-      shortcutCode.insertAdjacentText('beforeend', ` ${shortcut.description}`);
-      shortcutListItem.appendChild(shortcutCode);
-      shortcutsList.appendChild(shortcutListItem);
-    });
-    return shortcutsList;
+  const shortcutsList = document.createElement("ul");
+  shortcuts.forEach((shortcut) => {
+    const shortcutListItem = document.createElement("li");
+    const shortcutCode = document.createElement("code");
+    const keyBinding = document.createElement("span");
+    const key = document.createElement("span");
+    key.className = "key";
+    key.textContent = shortcut.key;
+    keyBinding.className = "key-binding";
+    keyBinding.appendChild(key);
+    shortcutCode.appendChild(keyBinding);
+    shortcutCode.insertAdjacentText("beforeend", ` ${shortcut.description}`);
+    shortcutListItem.appendChild(shortcutCode);
+    shortcutsList.appendChild(shortcutListItem);
+  });
+  return shortcutsList;
 }
-  
-  const shortcuts = [
-    { key: '>', description: 'Increase playback speed by 0.1' },
-    { key: '<', description: 'Decrease playback speed by 0.1' },
-    { key: '+', description: 'Increase playback speed by 0.5' },
-    { key: '-', description: 'Decrease playback speed by 0.5' },
-    { key: 'ArrowUp (↑)', description: 'Increase volume by 0.1' },
-    { key: 'ArrowDown (↓)', description: 'Decrease volume by 0.1' },
-    { key: 'ArrowLeft (←)', description: 'Seek backward 10 seconds' },
-    { key: 'ArrowRight (→)', description: 'Seek forward 10 seconds' },
-    { key: 'p', description: 'Request Picture in Picture' },
-    { key: 'P', description: 'Exit Picture in Picture' },
-    { key: 'j', description: 'Seek backward 5 seconds' },
-    { key: 'Space / k', description: 'Toggle play/pause' },
-    { key: 'l', description: 'Seek forward 5 seconds' },
-    { key: ',', description: 'Previous frame' },
-    { key: '.', description: 'Next frame' },
-    { key: 'Home', description: 'Seek to the beginning of the video' },
-    { key: 'End', description: 'Seek to the end of the video' },
-    { key: '0-9', description: 'Seek to a percentage of the video'},
-    { key: '?', description: 'Toggle keyboard shortcuts' },
-    { key: 'Escape', description: 'Close keyboard shortcuts' },
-    { key: 'f', description: 'Toggle fullscreen'},
-    { key: 'm', description: 'Toggle mute'},
-  ];
-  
-  const shortcutsContainer = document.getElementById('keyboard-shortcuts');
-  shortcutsContainer.appendChild(generateShortcutsDocumentation(shortcuts));
 
+const shortcuts = [
+  { key: "<", description: "Decrease playback speed by 0.1" },
+  { key: ">", description: "Increase playback speed by 0.1" },
+  { key: "-", description: "Decrease playback speed by 0.5" },
+  { key: "+", description: "Increase playback speed by 0.5" },
+  { key: "ArrowDown (↓)", description: "Decrease volume by 0.1" },
+  { key: "ArrowUp (↑)", description: "Increase volume by 0.1" },
+  { key: "ArrowLeft (←)", description: "Seek backward 10 seconds" },
+  { key: "ArrowRight (→)", description: "Seek forward 10 seconds" },
+  { key: "p", description: "Request Picture in Picture" },
+  { key: "P", description: "Exit Picture in Picture" },
+  { key: "j", description: "Seek backward 5 seconds" },
+  { key: "Space / k", description: "Toggle play/pause" },
+  { key: "l", description: "Seek forward 5 seconds" },
+  { key: ",", description: "Previous frame" },
+  { key: ".", description: "Next frame" },
+  { key: "Home", description: "Seek to the beginning of the video" },
+  { key: "End", description: "Seek to the end of the video" },
+  { key: "0-9", description: "Seek to a percentage of the video" },
+  { key: "Esc", description: "Close keyboard shortcuts" },
+  { key: "?", description: "Toggle keyboard shortcuts" },
+  { key: "f", description: "Toggle fullscreen" },
+  { key: "m", description: "Toggle mute" },
+];
 
-
+const shortcutsContainer = document.getElementById("keyboard-shortcuts");
+shortcutsContainer.appendChild(generateShortcutsDocumentation(shortcuts));
 
 let modal = document.getElementById("keyboard-shortcuts");
 
