108aa64299f32371afd768b8445386473f5e7e6a
bishal-dahal <101317575+bishaldahal@users.noreply.github.com>
2024-03-02 15:56:46 +0545

    Update Chrome API to Browser API in background.js and event.js

--- js/background.js
+++ js/background.js
@@ -1,17 +1,17 @@
-// chrome.runtime.onInstalled.addListener(() => {
-  chrome.webNavigation.onBeforeNavigate.addListener((details) => {
+// browser.runtime.onInstalled.addListener(() => {
+  browser.webNavigation.onBeforeNavigate.addListener((details) => {
     if (details.frameId === 0 && details.url.endsWith('.m3u8')) {
-      const playerUrl = chrome.runtime.getURL('player.html') + '#' + details.url;
+      const playerUrl = browser.runtime.getURL('player.html') + '#' + details.url;
       console.log("Before Navigation", playerUrl);
-      chrome.tabs.update({ url: playerUrl });
+      browser.tabs.update({ url: playerUrl });
     }
   }, { urls: ["<all_urls>","*://*/*.m3u8"] }, ["blocking"]);
 // });
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.command == 'PLAY_M3U8') {
-    var playerUrl = chrome.runtime.getURL('player.html') + "#" + request.url;
+    var playerUrl = browser.runtime.getURL('player.html') + "#" + request.url;
     console.log("onMessage", playerUrl);
-    chrome.tabs.create({ url: playerUrl });
+    browser.tabs.create({ url: playerUrl });
   }
 });
--- js/event.js
+++ js/event.js
@@ -1,16 +1,16 @@
 var enabled = true;
 
-chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
+browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.command == 'PLAY_M3U8') {
-        var playerUrl = chrome.extension.getURL('player.html') + "#" + request.url
-        chrome.tabs.create({url: playerUrl});
+        var playerUrl = browser.runtime.getURL('player.html') + "#" + request.url
+        browser.tabs.create({url: playerUrl});
     }
 });
 
-chrome.webRequest.onBeforeRequest.addListener(
+browser.webRequest.onBeforeRequest.addListener(
     function (info) {
         if (enabled && info.type == "main_frame" && info.url.split("?")[0].split("#")[0].endsWith("m3u8")) {
-            var playerUrl = chrome.extension.getURL('player.html') + "#" + info.url
+            var playerUrl = browser.runtime.getURL('player.html') + "#" + info.url
             return {redirectUrl: playerUrl}
         }
     },
