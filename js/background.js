920fd8b084d9003e58fc79bc4672e41db3a68b17
bishal-dahal <101317575+bishaldahal@users.noreply.github.com>
2024-03-02 16:25:20 +0545

    Make background script compatible with firefox

--- js/background.js
+++ js/background.js
@@ -1,16 +1,18 @@
-// browser.runtime.onInstalled.addListener(() => {
-  browser.webNavigation.onBeforeNavigate.addListener((details) => {
-    if (details.frameId === 0 && details.url.endsWith('.m3u8')) {
-      const playerUrl = browser.runtime.getURL('player.html') + '#' + details.url;
-      console.log("Before Navigation", playerUrl);
-      browser.tabs.update({ url: playerUrl });
-    }
-  }, { urls: ["<all_urls>","*://*/*.m3u8"] }, ["blocking"]);
-// });
+browser.webNavigation.onBeforeNavigate.addListener((details) => {
+  if (
+    details.frameId === 0 &&
+    details.url.endsWith(".m3u8") &&
+    !details.url.startsWith(browser.runtime.getURL(""))
+  ) {
+    const playerUrl = browser.runtime.getURL("player.html") + "#" + details.url;
+    console.log("Before Navigation", playerUrl);
+    browser.tabs.update(details.tabId, { url: playerUrl });
+  }
+});
 
 browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.command == 'PLAY_M3U8') {
-    var playerUrl = browser.runtime.getURL('player.html') + "#" + request.url;
+  if (request.command == "PLAY_M3U8") {
+    var playerUrl = browser.runtime.getURL("player.html") + "#" + request.url;
     console.log("onMessage", playerUrl);
     browser.tabs.create({ url: playerUrl });
   }
