ebfd6242fd287949cb491d659311a6848f99ba5f
bishal-dahal <101317575+bishaldahal@users.noreply.github.com>
2024-04-03 15:54:48 +0545

    Add script to generate source code for Firefox

---
+++ build_scripts/generate_source_firefox.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const { execSync } = require('child_process');
+
+// Get current date
+const date = new Date();
+const formattedDate = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
+
+// Define the output directory and filename
+const outputDir = path.resolve(__dirname, '..', 'build');
+const outputFile = path.join(outputDir, `source-code.Firefox.${formattedDate}.zip`);
+
+// Ensure the output directory exists
+fs.mkdirSync(outputDir, { recursive: true });
+
+// Delete old zip files
+const oldZipFiles = fs.readdirSync(outputDir).filter(file => file.endsWith('.zip'));
+for (const file of oldZipFiles) {
+  fs.unlinkSync(path.join(outputDir, file));
+}
+
+// Define the 7z command
+const command = `"Z:\\Programs\\7-Zip\\7z" a -tzip -r "${outputFile}" . -x!node_modules -x!.vscode -x!original -x!package-lock.json -x!README.md -x!extension.zip -x!.git -x!.gitignore -x!build_scripts -x!build -x!.github -x!.gitattributes -x!LICENSE -x!.vscode`;
+// const command = `7z a -tzip -r "${outputFile}" . -x!node_modules -x!.vscode -x!npm -x!package.json -x!package-lock.json -x!README.md -x!extension.zip -x!.git -x!.gitignore -x!build_scripts -x!build -x!.github -x!.gitattributes -x!LICENSE -x!.vscode`;
+
+// Execute the command
+execSync(command, { stdio: 'inherit' });
