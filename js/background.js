a68d32128f74de79657f8df9935d52de1fba3bdd
bishal-dahal <101317575+bishaldahal@users.noreply.github.com>
2024-04-03 15:27:09 +0545

    Comment out console.log statements in background.js

--- js/background.js
+++ js/background.js
@@ -2,7 +2,7 @@
   chrome.webNavigation.onBeforeNavigate.addListener((details) => {
     if (details.frameId === 0 && new URL(details.url).pathname.endsWith(".m3u8")) {
       const playerUrl = chrome.runtime.getURL('player.html') + '#' + details.url;
-      console.log("Before Navigation", playerUrl);
+      // console.log("Before Navigation", playerUrl);
       chrome.tabs.update({ url: playerUrl });
     }
   }, { urls: ["<all_urls>","*://*/*.m3u8"] }, ["blocking"]);
@@ -11,7 +11,7 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.command == 'PLAY_M3U8') {
     var playerUrl = chrome.runtime.getURL('player.html') + "#" + request.url;
-    console.log("onMessage", playerUrl);
+    // console.log("onMessage", playerUrl);
     chrome.tabs.create({ url: playerUrl });
   }
 });
